c58226123aee6f01eea29ba2c83125781a62fd79
narges shafazadeh <n.shafazadeh@gmail.com>
2026-02-17 22:39:36 +0330

    delete userAgent middleware

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,14 +1,9 @@
-import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UserAgentMiddleware } from '../common/middleware/user-agent.middleware';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
 })
-export class AuthModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(UserAgentMiddleware).forRoutes('auth');
-  }
-}
+export class AuthModule {}
